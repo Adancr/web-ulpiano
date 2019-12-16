d8e26beb4c2e99d0a0685ae778c9e90356a0e8d9
AdanCR <adsadancr@gmail.com>
2019-12-15 20:30:23 -0500

    Iniciando slider por js

---
+++ js/main.js
@@ -0,0 +1,18 @@
+/*-------------------------------
+Iniciando Swiper
+-------------------------------*/
+// var swiper =new swiper('#animacion',{
+
+ 
+//   navigation: {
+//     nexEl: '.swiper-button-next',
+//     prevEl: '.swiper-button-prev',
+//   }
+// });;
+
+var mySwiper = new Swiper('#animacion', {
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
